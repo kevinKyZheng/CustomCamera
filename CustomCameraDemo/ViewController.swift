204a0b2f6d9b4fbe313549b3e5d2556680117adb
zhengkaiyuan <zhengkaiyuan@chinawutong.com>
2018-10-19 15:12:43 +0800

    初始化
    初始化

---
+++ CustomCameraDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomCameraDemo
+//
+//  Created by ios on 2018/10/19.
+//  Copyright © 2018年 KY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
